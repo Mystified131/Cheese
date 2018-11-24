0625dd748728e508e71336c998f0299844b88613
mystified131 <mystifiedthomas@gmail.com>
2018-11-24 02:48:49 -0600

    Adding validation, image, other touch-ups

--- Cheese.py
+++ Cheese.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session, redirect
+from flask import Flask, request, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 import datetime, os, cgi
 
@@ -20,37 +20,60 @@ class Cheese(db.Model):
         self.name = name
         self.description = description
 
-@app.route("/", methods =['GET'])
-def main():
-     right_now = datetime.datetime.now().isoformat()
-     list = []
+@app.route("/", methods =['GET', 'POST'])
+def index():
+    right_now = datetime.datetime.now().isoformat()
+    list = []
 
-     for i in right_now:
+    for i in right_now:
         if i.isnumeric():
            list.append(i)
 
-     tim = "".join(list)
-     session['timestamp'] = tim
-     return redirect ('/index')
-
-@app.route("/index", methods =['GET', 'POST'])
-def index():
+    tim = "".join(list)
+    session['timestamp'] = tim
     cheeses = Cheese.query.all()
-    return render_template('index.html', cheeses = cheeses, time = session['timestamp'])
+    cheeselist = []
+    for cheese in cheeses:
+        cheesestr = cheese.name + ": " + cheese.description
+        cheeselist.append(cheesestr)
+    cheeselist.sort()
+    return render_template('index.html', cheeses = cheeselist)
 
 
 @app.route("/add", methods =['GET', 'POST'])
 def add():
+    error = ""
     cheesename = request.form["name"]
     cheesedescript = request.form["descript"]
     timestamp = session['timestamp']
     name = cgi.escape(cheesename)
+    name = name.lower()
     description = cgi.escape(cheesedescript)
+    old_cheese = Cheese.query.filter_by(name=name).first()
+    if old_cheese or not name or not description:
+        if not description:
+            error = "Please describe the cheese, in order to add it."
+        if not name:
+            error = "There is no cheese with no name."
+        if old_cheese:
+            error = "That cheese is already in the database."
+        cheeses = Cheese.query.all()
+        cheeselist = []
+        for cheese in cheeses:
+            cheesestr = cheese.name + ": " + cheese.description
+            cheeselist.append(cheesestr)
+        cheeselist.sort()
+        return render_template('index.html', cheeses = cheeselist, error = error)
     new_cheese = Cheese(timestamp, name, description)
     db.session.add(new_cheese)
     db.session.commit()
     cheeses = Cheese.query.all()
-    return render_template('index.html', cheeses = cheeses, time = session['timestamp'])
+    cheeselist = []
+    for cheese in cheeses:
+        cheesestr = cheese.name + ": " + cheese.description
+        cheeselist.append(cheesestr)
+    cheeselist.sort()
+    return render_template('index.html', cheeses = cheeselist)
 
 @app.route("/remove", methods =['GET', 'POST'])
 def remove():
@@ -60,8 +83,22 @@ def remove():
     if the_cheese:
         db.session.delete(the_cheese)
         db.session.commit()
-    cheeses = Cheese.query.all()
-    return render_template('index.html', cheeses = cheeses, time = session['timestamp'])
+        cheeses = Cheese.query.all()
+        cheeselist = []
+        for cheese in cheeses:
+            cheesestr = cheese.name + ": " + cheese.description
+            cheeselist.append(cheesestr)
+        cheeselist.sort()
+        return render_template('index.html', cheeses = cheeselist)
+    else:
+        error2 = "That cheese is not in the database."
+        cheeses = Cheese.query.all()
+        cheeselist = []
+        for cheese in cheeses:
+            cheesestr = cheese.name + ": " + cheese.description
+            cheeselist.append(cheesestr)
+        cheeselist.sort()
+        return render_template('index.html', cheeses = cheeselist, error2 = error2)
 
 if __name__ == '__main__':
     app.run()
